e990a97cf35779e2ba3372191812ced10b1f9c92
pancha04 <panchaponto07@gmail.com>
2025-09-20 11:13:58 -0300

    cambios

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const expres= require('express');
+const productsRouter= require('../routes/productsRouter');
+const cartsRouter= require('../routes/cartsRouter');
+
+const PORT=8080;
+app.listen(PORT, ()=>{
+    console.log(`server escuchando en http://localhost:${PORT}`)
+})
